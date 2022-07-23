ff23bf096b931d93051289a90e9e64561db8c01d
Mason Benzinger <mason.benzinger@gmail.com>
2022-07-23 12:53:09 -0500

    commiting

---
+++ controllers/breads_controllers.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const breads = express.Router()
+const Bread = require('../models/bread.js')
+
+// INDEX
+breads.get('/', (req, res) => {
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+  // res.send(Bread)
+})
+
+
+
+module.exports = breads
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+  })
+  
+
+/* ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  */
